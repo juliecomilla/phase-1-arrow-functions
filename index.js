7bdcf327c01b3c7a79e7c0327bd540723b312ee8
Julie <juliecomilla@gmail.com>
2023-08-27 15:35:44 -0400

    completed files

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide(parameter1,parameter2) {
+    return parameter1 / parameter2
+}
+divide(2000,100 );
+
+const square = square => {
+    return square * square
+}
+square(1);
+
+const add = (num1,num2) => {
+return num1 + num2
+
+}
+add(2,2);
